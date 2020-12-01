4afca47c076ad237444dd17ad2035850644402bc
Pavel Tuma <pavel.tuma@yahoo.com>
2020-12-01 07:57:45 +0100

    Do nastroje isir-ws pridan prepinac --last-id

--- pdf-scraper/isir_explorer/webservice/cli.py
+++ pdf-scraper/isir_explorer/webservice/cli.py
@@ -50,8 +50,12 @@ def validate_config_file(ctx, param, value):
               default='app.cfg',
               type=click.File('r'),
               callback=validate_config_file)
+@click.option('-l', '--last-id',
+              help='ID posledního archivovaného záznamu - od tohoto ID začne stahování nových záznamů. Pokud není uvedeno, použije se MAX(isir_udalost.id) hodnota z databáze.',
+              type=click.IntRange(min=-1, max=None))
+def isirClient(config, last_id):
 
-def isirClient(config):
+    config.set_opt("last_id", last_id)
 
     app = IsirClient(config)
     app.start_loop()
