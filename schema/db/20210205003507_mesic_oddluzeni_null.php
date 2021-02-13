1dfaaf2c4d730cb718c086581a1e35ba70731f01
Pavel Tuma <pavel.tuma@yahoo.com>
2021-02-13 22:00:54 +0100

    Povoleni null i u dalsich sloupcu zplo_vykaz_plneni

--- schema/db/20210205003507_mesic_oddluzeni_null.php
+++ schema/db/20210205003507_mesic_oddluzeni_null.php
@@ -13,6 +13,19 @@ final class MesicOddluzeniNull extends AbstractMigration
         $table = $this->table('zplo_vykaz_plneni');
         $table->changeColumn('mesic', 'smallinteger', ['null' => true])
                ->changeColumn('mesic_oddluzeni', 'smallinteger', ['null' => true])
+
+               ->changeColumn('prijem', 'decimal', ['null' => true, 'scale' => DEC_SCAL, 'precision' => DEC_PREC])
+               ->changeColumn('srazky', 'decimal', ['null' => true, 'scale' => DEC_SCAL, 'precision' => DEC_PREC])
+               ->changeColumn('zm_nnb', 'decimal', ['null' => true, 'scale' => DEC_SCAL, 'precision' => DEC_PREC])
+               ->changeColumn('nepostizitelne', 'decimal', ['null' => true, 'scale' => DEC_SCAL, 'precision' => DEC_PREC])
+               ->changeColumn('postizitelne', 'decimal', ['null' => true, 'scale' => DEC_SCAL, 'precision' => DEC_PREC])
+               ->changeColumn('vraceno_dluznikum', 'decimal', ['null' => true, 'scale' => DEC_SCAL, 'precision' => DEC_PREC])
+               ->changeColumn('mimoradny_prijem', 'decimal', ['null' => true, 'scale' => DEC_SCAL, 'precision' => DEC_PREC])
+               ->changeColumn('darovaci_smlouva', 'decimal', ['null' => true, 'scale' => DEC_SCAL, 'precision' => DEC_PREC])
+               ->changeColumn('odmena_is', 'decimal', ['null' => true, 'scale' => DEC_SCAL, 'precision' => DEC_PREC])
+               ->changeColumn('vyzivne', 'decimal', ['null' => true, 'scale' => DEC_SCAL, 'precision' => DEC_PREC])
+               ->changeColumn('ostatnim_veritelum', 'decimal', ['null' => true, 'scale' => DEC_SCAL, 'precision' => DEC_PREC])
+
                ->save();
     }
 
@@ -24,6 +37,19 @@ final class MesicOddluzeniNull extends AbstractMigration
         $table = $this->table('zplo_vykaz_plneni');
         $table->changeColumn('mesic', 'smallinteger', ['null' => false])
                ->changeColumn('mesic_oddluzeni', 'smallinteger', ['null' => false])
+
+               ->changeColumn('prijem', 'decimal', ['null' => false, 'scale' => DEC_SCAL, 'precision' => DEC_PREC])
+               ->changeColumn('srazky', 'decimal', ['null' => false, 'scale' => DEC_SCAL, 'precision' => DEC_PREC])
+               ->changeColumn('zm_nnb', 'decimal', ['null' => false, 'scale' => DEC_SCAL, 'precision' => DEC_PREC])
+               ->changeColumn('nepostizitelne', 'decimal', ['null' => false, 'scale' => DEC_SCAL, 'precision' => DEC_PREC])
+               ->changeColumn('postizitelne', 'decimal', ['null' => false, 'scale' => DEC_SCAL, 'precision' => DEC_PREC])
+               ->changeColumn('vraceno_dluznikum', 'decimal', ['null' => false, 'scale' => DEC_SCAL, 'precision' => DEC_PREC])
+               ->changeColumn('mimoradny_prijem', 'decimal', ['null' => false, 'scale' => DEC_SCAL, 'precision' => DEC_PREC])
+               ->changeColumn('darovaci_smlouva', 'decimal', ['null' => false, 'scale' => DEC_SCAL, 'precision' => DEC_PREC])
+               ->changeColumn('odmena_is', 'decimal', ['null' => false, 'scale' => DEC_SCAL, 'precision' => DEC_PREC])
+               ->changeColumn('vyzivne', 'decimal', ['null' => false, 'scale' => DEC_SCAL, 'precision' => DEC_PREC])
+               ->changeColumn('ostatnim_veritelum', 'decimal', ['null' => false, 'scale' => DEC_SCAL, 'precision' => DEC_PREC])
+
                ->save();
     }
 }
