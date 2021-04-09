f2049026799f710edd7d7ec17c1761f942734e33
Pavel Tuma <pavel.tuma@yahoo.com>
2021-04-09 12:05:37 +0200

    Skryta funkce hledani

--- isir-explorer/resources/views/layouts/app.blade.php
+++ isir-explorer/resources/views/layouts/app.blade.php
@@ -55,10 +55,6 @@
                         </div>
                     </li>
                 </ul>
-                <form class="form-inline my-2 my-lg-0">
-                    <input class="form-control mr-sm-2" type="text" placeholder="Hledat">
-                    <button class="btn btn-secondary my-2 my-sm-0" type="submit">Hledat</button>
-                </form>
             </div>
         </div>
     </nav>
