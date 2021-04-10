415bebfdb31b365460fa8366a0b538a7320c15c8
Pavel Tuma <pavel.tuma@yahoo.com>
2021-04-10 17:00:18 +0200

    Komentare v docker-compose.sh

--- isir-explorer/docker-compose.sh
+++ isir-explorer/docker-compose.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
+
+# Sestaveni
 docker-compose build
+
+# Instalace PHP zavislosti
 docker-compose run --rm isir_composer install
+
+# Instalace JS zavislosti
 docker-compose run --rm isir_npm install --cache /tmp
+
+# Kompilace SASS, sestaveni a minifikace JS a CSS souboru
 docker-compose run --rm isir_npm run prod
+
+# Aktualizace nastaveni z .env
 docker-compose run --rm isir_artisan config:clear
+
+# Vytvoreni cache
 docker-compose run --rm isir_artisan responsecache:statistiky
+
+# Spusteni webserveru
 docker-compose up
